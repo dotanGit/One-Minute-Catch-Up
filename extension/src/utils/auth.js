5dc1f0d590e050504c663282a7f4c4770437910d
dotanGit <frenkeldotan1998@gmail.com>
2025-04-29 14:23:18 +0300

    Switch google client iD

--- extension/src/utils/auth.js
+++ extension/src/utils/auth.js
@@ -2,6 +2,7 @@ export async function getAuthToken() {
     return new Promise((resolve, reject) => {
       chrome.identity.getAuthToken({ interactive: false }, (token) => {
         if (chrome.runtime.lastError) {
+          console.error('[BG] ❌ Token received:', token);
           console.error('Auth error:', chrome.runtime.lastError);
           reject(chrome.runtime.lastError);
         } else {
