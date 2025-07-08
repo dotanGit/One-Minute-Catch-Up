5da575646aa7501d8dbac4c6145fee0025ccf3da
dotanGit <frenkeldotan1998@gmail.com>
2025-07-08 13:48:42 +0300

    Remove keys from tracking

--- extension/keys.js
+++
@@ -1,5 +0,0 @@
-export const API_KEYS{
-    OPENAI_API_KEY="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",
-    GOOGLE_CLIENT_KEY="1050975138446-fnfd6iu6890ul4hbm334t3aqk4cpmcmd.apps.googleusercontent.com"
-};
-
